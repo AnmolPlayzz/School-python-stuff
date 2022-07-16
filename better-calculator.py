74283ba23c4ae1de65dbc4392c9fa8ff2adabc6e
AnmolPlayzz <anmolisnotavailable@gmail.com>
2022-07-16 11:07:24 +0000

    Stuff

---
+++ better-calculator.py
@@ -0,0 +1,26 @@
+#Not officially done in school, but I did it in my free time.
+
+print("Preffer entering larger number first\n")
+
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+
+print("\nSlect an operation from the following:\n\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n\n")
+
+operator=input("Enter operator:")
+
+if operator=="Add" or operator=="Subtract" or operator=="Multiply" or operator=="Divide":
+    if operator=="Add":
+        print("Sum:", a+b)
+
+    if operator=="Subtract":
+        print("Difference:", a-b)
+
+    if operator=="Multiply":
+        print("Product:", a*b)
+
+    if operator=="Divide":
+        print("Quotient:", a/b)
+
+else:
+    print("Invalid operator")
---
+++ calculator.py
@@ -0,0 +1,11 @@
+print("Preffer entering larger number first\n")
+
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+
+print("\nSun:", a+b)
+print("Differance:", a-b)
+print("Product:", a*b)
+print("Quotient:", a/b)
+print("Remainder:", a%b)
+print("Exponent:", a**b)

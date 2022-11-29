8b228f1c852f4d721d8cc2f7a3dd636f0fbae80e
AnmolPlayzz <74979911+AnmolPlayzz@users.noreply.github.com>
2022-11-29 15:41:51 +0530

    💀 I messed up

--- min-max-list.py
+++ min-max-list.py
@@ -1,11 +1,11 @@
 l=eval(input("Enter list -> "))
 maxm=0
 for i in l:
-    if l>maxm:
-        maxm=l
+    if i>maxm:
+        maxm=i
 minm=maxm
 for j in l:
-    if l<minm:
-        minm=l
+    if j<minm:
+        minm=j
 print("Maximum ->", maxm)
 print("Minimum ->", minm)
